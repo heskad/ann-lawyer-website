bece76586def9eb3205843b94ad10fd69b5cc4b0
Ruslan V. Smetanin <ruslan.smetanin.666@gmail.com>
2025-06-20 22:30:16 +0000

    ты обрезал на фото лоб, сдвинь фото в низ, чтобы лицо поместиллось полно

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -29,7 +29,7 @@ export function HeroSection() {
             width={500}
             height={500}
             className="rounded-full object-cover aspect-square shadow-2xl"
-            style={{ objectPosition: 'center 40%' }}
+            style={{ objectPosition: 'center 30%' }}
           />
         </div>
       </section>
